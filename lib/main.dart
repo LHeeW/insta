2d019cfa4554960ddaddf57bc1628e79481852d6
LHW <LHeeW>
2023-01-26 15:36:46 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,79 @@
 import 'package:flutter/material.dart';
-import 'style.dart' as style;
+import './style.dart' as style;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-      const MaterialApp(
-        home: MyApp(),
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (c) => Store1()),
+          ChangeNotifierProvider(create: (c) => Store2()),
+        ],
+        child: MaterialApp(
+          home: const MyApp(),
+          theme: style.theme,
+        ),
       )
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int tab = 0;
+  var data = [];
+  var userImage;
+  var userContent;
+
+  addMyData(){
+    var myData = {
+      'id': data.length,
+      'image': userImage,
+      'likes': 5,
+      'date': 'July 25',
+      'content': userContent,
+      'liked': false,
+      'user': 'John Kim'
+    };
+    setState(() {
+      data.insert(0, myData);
+    });
+  }
+
+  setUserContent(a){
+    setState(() {
+      userContent = a;
+    });
+  }
+
+  addData(a){
+    setState(() {
+      data.add(a);
+    });
+  }
+
+  getData() async {
+    var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
+    var result2 = jsonDecode(result.body);
+    setState((){
+      data = result2;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,9 +81,209 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Instagram'),
         actions: [
-          IconButton(icon: const Icon(Icons.add_box_outlined), onPressed: (){},),
+          IconButton(icon: const Icon(Icons.add_box_outlined), onPressed: () async{
+            var picker = ImagePicker();
+            var image = await picker.pickImage(source: ImageSource.gallery);
+            if (image != null) {
+              setState((){
+                userImage = File(image.path);
+              });
+            }
+
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) =>
+                    Upload(setUserContent:setUserContent,addMyData:addMyData,userImage:userImage)));
+          },),
+        ],
+      ),
+      body: [Home(data: data,addData: addData),const Text('Shop')][tab],
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        onTap: (value){
+          setState(() {
+            tab = value;
+          });
+        },
+        currentIndex: tab,
+        items: const[
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined),activeIcon: Icon(Icons.home),label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag_outlined),activeIcon: Icon(Icons.shopping_bag),label: 'Shop'),
         ],
       ),
     );
   }
 }
+
+class Home extends StatefulWidget {
+  const Home({Key? key,this.data,this.addData}) : super(key: key);
+  final data;
+  final addData;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var scroll = ScrollController();
+
+  getMore() async {
+    var result = await http.get(Uri.parse('https://codingapple1.github.io/app/more1.json'));
+    var result2 = jsonDecode(result.body);
+    widget.addData(result2);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    scroll.addListener(() {
+      if (scroll.position.pixels == scroll.position.maxScrollExtent){
+        getMore();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(widget.data.isNotEmpty){
+      return ListView.builder(itemCount: widget.data.length,controller: scroll,itemBuilder: (context,i){
+        return Column(
+          children: [
+            Container(
+              constraints: const BoxConstraints(maxWidth: 600),
+              padding: const EdgeInsets.all(20),
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  widget.data[i]['image'].runtimeType == String
+                      ? Image.network(widget.data[i]['image'])
+                      : Image.file(widget.data[i]['image']),
+                  GestureDetector(
+                      child: Text(widget.data[i]['user']),
+                    onTap: (){
+                        Navigator.push(context,
+                          MaterialPageRoute(builder: (c) => Profile())
+                        );
+                    }
+                  ),
+                  Text('좋아요: ${widget.data[i]['likes']}'),
+                  Text(widget.data[i]['date']),
+                  Text(widget.data[i]['content']),
+                ],
+              ),
+            )
+          ],
+        );
+      });
+    }else {
+      return const Text('로딩중');
+    }
+  }
+}
+
+class Upload extends StatelessWidget {
+  const Upload({Key? key,this.setUserContent,this.addMyData,this.userImage}) : super(key: key);
+  final setUserContent;
+  final addMyData;
+  final userImage;
+
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar( actions: [
+          IconButton(onPressed: (){addMyData();}, icon: const Icon(Icons.send))
+        ]),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.file(userImage),
+              Text('이미지업로드화면'),
+              TextField(onChanged: (text){ setUserContent(text); }),
+              IconButton(
+                  onPressed: ()=>Navigator.pop(context),
+                  icon: const Icon(Icons.close)
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
+
+class Store1 extends ChangeNotifier {
+  var friend = false;
+  var follower = 0;
+  var profileImage = [];
+
+  getData() async {
+    var result = await http.get(Uri.parse('https://codingapple1.github.io/app/profile.json'));
+    var result2 = jsonDecode(result.body);
+    profileImage = result2;
+    notifyListeners();
+  }
+
+  addFollower(){
+    if (friend == false) {
+      follower++;
+      friend = true;
+    } else {
+      follower--;
+      friend = false;
+    }
+    notifyListeners();
+  }
+}
+class Store2 extends ChangeNotifier{
+  var name = 'john kim';
+}
+
+class Profile extends StatelessWidget {
+  const Profile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(context.watch<Store2>().name),),
+      body: CustomScrollView(
+        slivers: [
+          const SliverToBoxAdapter(child: ProfileHeader()),
+          SliverGrid(
+              delegate: SliverChildBuilderDelegate(
+                    (c,i) => Image.network(context.watch<Store1>().profileImage[i]),
+                childCount: context.watch<Store1>().profileImage.length,
+              ),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount( crossAxisCount: 2 ))
+        ],
+      )
+    );
+  }
+}
+
+class ProfileHeader extends StatelessWidget {
+  const ProfileHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        const CircleAvatar(
+          radius: 30,
+          backgroundColor: Colors.grey,
+        ),
+        Text('팔로워 ${context.watch<Store1>().follower}명'),
+        ElevatedButton(onPressed: (){
+          context.read<Store1>().addFollower();
+        }, child: const Text('팔로우')),
+        ElevatedButton(onPressed: (){
+          context.read<Store1>().getData();
+        }, child: const Text('사진가져오기')),
+      ],
+    );
+  }
+}
+
--- lib/style.dart
+++ lib/style.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
 ThemeData theme = ThemeData(
-
+  appBarTheme: const AppBarTheme(
+    color: Colors.white,
+    elevation: 1,
+    titleTextStyle: TextStyle(color: Colors.black, fontSize: 25),
+    actionsIconTheme: IconThemeData(color: Colors.black,size: 40),
+  ),
+  textTheme: const TextTheme(
+    bodyText2: TextStyle(color: Colors.black),
+  ),
+  bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+    backgroundColor: Colors.white,
+    elevation: 2,
+    selectedItemColor: Colors.black,
+  ),
 );
